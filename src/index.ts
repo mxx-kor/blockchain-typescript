0aa8ac675ae7521f3ea750f8c7402f41d964823a
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-10-06 19:12:00 +0900

    0.13 Conclusions

--- src/index.ts
+++ src/index.ts
@@ -65,6 +65,7 @@ const createNewBlock = (data: string): Block => {
         data,
         newTimestamp
     );
+    addBlock(newBlock);
     return newBlock;
 };
 
@@ -95,6 +96,12 @@ const addBlock = (candidateBlock: Block): void => {
         blockchain.push(candidateBlock);
     }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 
 export {};
